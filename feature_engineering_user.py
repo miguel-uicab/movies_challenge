b1074703b4e28f845f3ec78a6979b33250b71b20
miguel-uicab <miguel.uicab.perera@gmail.com>
2021-11-28 01:15:02 -0600

    Se sube jupyter training

--- feature_engineering_user.py
+++ feature_engineering_user.py
@@ -1,125 +1,95 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Imports
-import os
-import re
-# os.chdir(f'{os.getcwd()}/../movies_challenge')
 import pandas as pd
 import numpy as np
 import pickle
-from functools import reduce
+import logging
 from extras import get_config
-config = get_config()
-
-# Data de entrenamiento completa #####################################################
-data_train = pickle.load(open('rating_redundant_train_2.sav',
-                              'rb'))
-
-# Data de entramiento filtrada sin "duplicadas" ######################################
-data_train_no_duplicates = pickle.load(open('rating_train_whithout_duplicate.sav',
-                                            'rb'))
-
-# Cargar las cluster encontrados en las películas ####################################
-movie_with_cluster = pickle.load(open('movie_with_cluster.sav',
-                                      'rb'))
-# data_train_no_duplicates[data_train_no_duplicates['userId']==85252]
-
-# Se comenzará la construcción con la data completa merge las peli con cluster #######
-# data_train_and_tag = pd.merge(data_train,
-#                               tag_info,
-#                               how="left",
-#                               on=["userId", "movieId"])
-
-data_train_and_cluster = pd.merge(data_train,
-                                  movie_with_cluster,
-                                  how="left",
-                                  on=["movieId"])
-data_train_and_cluster.reset_index(drop=True,
-                                   inplace=True)
-data_train_and_cluster['movies'] = 1
-
-data_train_and_cluster['userId'].value_counts()
+from past_information_user import past_information_user
+logging_format = '%(asctime)s %(levelname)s: %(message)s'
+logging.basicConfig(format=logging_format, datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO)
 
 
-# Tomar un ejemplo individual ######################################################
-userid=8405
-data_ind = data_train_and_cluster[data_train_and_cluster['userId']==userid]
+def total_past_extractor(is_data_train=True):
+    config = get_config()
+    logging.info('SE CARGAN LAS DATAS NECESARIAS.')
+    # Data redundante completa ################################################
+    if is_data_train:
+        data_total_path = 'rating_redundant_train_2.sav'
 
-names_interest_cum = ['time_day', 'movies', 'genre_crime',
-                      'genre_no genres listed', 'genre_adventure',
-                      'genre_romance', 'genre_horror', 'genre_drama',
-                      'genre_thriller', 'genre_comedy', 'genre_imax',
-                      'genre_documentary', 'genre_action', 'genre_musical',
-                      'genre_sci-fi', 'genre_animation', 'genre_fantasy',
-                      'genre_children', 'genre_western', 'genre_war',
-                      'genre_mystery', 'genre_film-noir']
+    else:
+        data_total_path = 'rating_redundant_test_1.sav'
+        # data_total_path = 'rating_redundant_test_1_movies_not_in_train.sav'
 
-info_movies_ind = data_ind[names_interest_cum]
+    data_total = pickle.load(open(data_total_path,
+                                  'rb'))
+    # Cargar las cluster encontrados en las películas ########################
+    movie_with_cluster = pickle.load(open('movie_with_cluster.sav',
+                                          'rb'))
 
-# Se colpasa la información por días ##########
-info_cumulative = (info_movies_ind.groupby(['time_day'])
-                                  .sum().groupby(level=0)
-                                  .cumsum()
-                                  .reset_index())
-new_names_interest_cum = []
-names_interest_cum.remove('time_day')
-for name in names_interest_cum:
-    new_name = f"frecc_{name}_per_day"
-    new_names_interest_cum.append(new_name)
-    info_cumulative.rename(columns={name: new_name}, inplace=True)
+    data_total_and_cluster = pd.merge(data_total,
+                                      movie_with_cluster,
+                                      how="left",
+                                      on=["movieId"])
+    data_total_and_cluster.reset_index(drop=True,
+                                       inplace=True)
+    data_total_and_cluster['movies'] = 1
+
+    logging.info('SE UBICAN LOS USERID DE INTERÉS.')
+    # list_total_user = [8405, 121535, 59477, 91577]  # <----train
+    # list_total_user = [89081, 102853, 79366]  # <----test
+    list_total_user = list(data_total_and_cluster['userId'].unique())
+    size_total_user = len(list_total_user)
+    iter_count = iter(range(1, size_total_user + 1))
+
+    # Tomar un ejemplo individual #############################################
+    logging.info('SE EXTRAE LA INFORMACIÓN DEL PASADO DE CADA USERID.')
+    list_datas_users = []
+    for userid in list_total_user:
+        iter_userid = next(iter_count)
+        past_info_f = past_information_user(data_with_cluster=data_total_and_cluster,
+                                            userId=userid)
+        list_datas_users.append(past_info_f)
+        logging.info(f'ITERACIÓN {iter_userid} DE UN TOTAL DE {size_total_user}.')
+
+    datas_users = pd.concat(list_datas_users)
+
+    logging.info('¡LISTO!')
+    # list_datas_users[0].shape
+    # list_datas_users[1].shape
+    # list_datas_users[2].shape
+    # list_datas_users[3].shape
+
+    ###########################################################################
+    logging.info('SE ADJUNTA A LA DATA CON CLUSTERS.')
+    if is_data_train:
+        # Data de entramiento filtrada sin "duplicadas" #######################
+        data_train_no_duplicates = pickle.load(open('rating_train_whithout_duplicate.sav',
+                                                    'rb'))
+
+        data_train_no_duplicates_info_past = pd.merge(data_train_no_duplicates,
+                                                      datas_users,
+                                                      how="left",
+                                                      on=["userId", "time_day"])
+        logging.info('SE GUARDA DATA DE ENTRENAMIENTO.')
+        data_path = 'final_data_train.sav'
+        pickle.dump(data_train_no_duplicates_info_past,
+                    open(data_path, 'wb'))
+    else:
+        data_total_info_past = pd.merge(data_total,
+                                        datas_users,
+                                        how="left",
+                                        on=["userId", "time_day"])
+        logging.info('SE GUARDA DATA DE PRUEBA.')
+        # data_path = 'final_data_test.sav'
+        data_path = 'final_data_test_complete.sav'
+        pickle.dump(data_total_info_past,
+                    open(data_path, 'wb'))
+
+    logging.info('FIN DE TODO EL PROCESO.')
 
 
-# Se tienen cantidades acumuladas ##############
-cumulative_names = []
-for name in new_names_interest_cum:
-    cumulative_name = f'cumulative{name}'
-    cumulative_names.append(cumulative_name)
-    info_cumulative[cumulative_name] = info_cumulative[name].cumsum()
-
-# Porcentaje de representación del género en el acumulado de películas ########
-cumulative_names.remove('cumulativefrecc_movies_per_day')
-percen_cumulative_names = []
-for name in cumulative_names:
-    percen_cumulative_name = f'percen{name}'
-    percen_cumulative_names.append(percen_cumulative_name)
-    info_cumulative[percen_cumulative_name] = info_cumulative[name] / info_cumulative['cumulativefrecc_movies_per_day']
-
-info_cumulative.sort_values(by='time_day',
-                            ascending=False,
-                            inplace=True)
-info_cumulative.reset_index(drop=True, inplace=True)
-date_column = info_cumulative[['time_day']]
-info_cumulative.rename(columns={'time_day': 'last_time_day'},
-                       inplace=True)
-
-info_cumulative.drop(0,
-                     axis=0,
-                     inplace=True)
-
-info_cumulative.reset_index(drop=True, inplace=True)
-
-past_info = pd.merge(date_column,
-                     info_cumulative,
-                     left_index=True,
-                     right_index=True,
-                     how="outer")
-
-past_info['userId'] = userid
-
-###############################################################################
-data_train_no_duplicates
-data_train_no_duplicates[data_train_no_duplicates['userId']==userid]
-
-
-data_train_no_duplicates_info_past =  pd.merge(data_train_no_duplicates,
-                                               past_info,
-                                               how="left",
-                                               on=["userId", "time_day"])
-
-data_train_no_duplicates_info_past.isna().sum()
-
-1002389
-data_train_no_duplicates_info_past[data_train_no_duplicates_info_past['userId']==userid]
-
-# data_except_current_month = data_interesting[data_interesting['month_date_in']<date(date.today().year, date.today().month, 1)]
+if __name__ == '__main__':
+    total_past_extractor()
